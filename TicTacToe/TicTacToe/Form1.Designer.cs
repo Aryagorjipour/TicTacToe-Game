24f8d56a1cbcecbc33cf7752105dd5a3105f713e
Aryagorjipour <arygorji.pour@gmail.com>
2021-08-17 20:36:26 +0430

    Uploaded the entire project

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtShow
+            // 
+            this.txtShow.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtShow.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtShow.Location = new System.Drawing.Point(12, 21);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ReadOnly = true;
+            this.txtShow.Size = new System.Drawing.Size(170, 13);
+            this.txtShow.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(12, 48);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(147, 109);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(163, 48);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(147, 109);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(314, 48);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(147, 109);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btn4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 161);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(147, 109);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(163, 161);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(147, 109);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btn6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(314, 161);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(147, 109);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(12, 274);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(147, 109);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(163, 274);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(147, 109);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(314, 274);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(147, 109);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnNewGame.Location = new System.Drawing.Point(390, 12);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 30);
+            this.btnNewGame.TabIndex = 10;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.BtnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(470, 389);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtShow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private bool _isTurn = true;
+        private int _turnCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+
+            button.Text = "X";
+            Value();
+
+            _isTurn = !_isTurn;
+
+            button.Enabled = false;
+
+            _turnCount++;
+            Win();
+        }
+
+        private void Win()
+        {
+            bool IsWin = false;
+
+
+            if ((btn1.Text == btn2.Text) && (btn2.Text == btn3.Text) && (!btn1.Enabled))
+                IsWin = true;
+            else if ((btn4.Text == btn5.Text) && (btn5.Text == btn6.Text) && (!btn4.Enabled))
+                IsWin = true;
+            else if ((btn7.Text == btn8.Text) && (btn8.Text == btn9.Text) && (!btn7.Enabled))
+                IsWin = true;
+            //-----------------------------------------------------------------
+            if ((btn1.Text == btn4.Text) && (btn4.Text == btn7.Text) && (!btn1.Enabled))
+                IsWin = true;
+            else if ((btn2.Text == btn5.Text) && (btn5.Text == btn8.Text) && (!btn2.Enabled))
+                IsWin = true;
+            else if ((btn3.Text == btn6.Text) && (btn6.Text == btn9.Text) && (!btn3.Enabled))
+                IsWin = true;
+            //----------------------------------------------------------------------------
+            if ((btn1.Text == btn5.Text) && (btn5.Text == btn9.Text) && (!btn1.Enabled))
+                IsWin = true;
+            else if ((btn3.Text == btn5.Text) && (btn5.Text == btn7.Text) && (!btn3.Enabled))
+                IsWin = true;
+
+
+
+            if (IsWin)
+            {
+                string Win;
+
+                if (_isTurn)
+                    Win = "X";
+                else
+                    Win = "O";
+
+                txtShow.Text = "Player " + Win + " Is Win !!!";
+
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btn3.Enabled = false;
+                btn4.Enabled = false;
+                btn5.Enabled = false;
+                btn6.Enabled = false;
+                btn7.Enabled = false;
+                btn8.Enabled = false;
+                btn9.Enabled = false;
+            }
+            else
+            {
+                if (_turnCount == 5)
+                {
+                    txtShow.Text = " OH!! Nooooo!! Draw !!!";
+                }
+            }
+        }
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            btn1.Enabled = true;
+            btn1.Text = "";
+            btn2.Enabled = true;
+            btn2.Text = "";
+            btn3.Enabled = true;
+            btn3.Text = "";
+            btn4.Enabled = true;
+            btn4.Text = "";
+            btn5.Enabled = true;
+            btn5.Text = "";
+            btn6.Enabled = true;
+            btn6.Text = "";
+            btn7.Enabled = true;
+            btn7.Text = "";
+            btn8.Enabled = true;
+            btn8.Text = "";
+            btn9.Enabled = true;
+            btn9.Text = "";
+            txtShow.Text = "";
+            _turnCount = 0;
+        }
+
+        private void Value()
+        {
+            if (btn5.Text == "")
+            {
+                btn5.Text = "O";
+                btn5.Enabled = false;
+            }
+            else
+            {
+                Prevent();
+            }
+
+        }
+        private void Prevent()
+        {
+            if (btn3.Text == "" && btn5.Text == "X")
+            {
+                btn3.Text = "O";
+                btn3.Enabled = false;
+            }
+            if (_turnCount >= 1)
+            {
+                if (btn5.Text == "X")
+                {
+                    ButtonFive();
+                }
+                else
+                {
+                    OtherButtons();
+                }
+            }
+        }
+        private void ButtonFive()
+        {
+            if (btn2.Text == btn5.Text && btn8.Text == "")
+            {
+                btn8.Text = "O";
+                btn8.Enabled = false;
+            }
+            else if (btn8.Text == btn5.Text && btn2.Text == "")
+            {
+                btn2.Text = "O";
+                btn2.Enabled = false;
+            }
+            else if (btn6.Text == btn5.Text && btn4.Text == "")
+            {
+                btn4.Text = "O";
+                btn4.Enabled = false;
+            }
+            else if (btn4.Text == btn5.Text && btn6.Text == "")
+            {
+                btn6.Text = "O";
+                btn6.Enabled = false;
+            }
+            else if (btn9.Text == btn5.Text && btn1.Text == "")
+            {
+                btn1.Text = "O";
+                btn1.Enabled = false;
+            }
+            else if (btn1.Text == btn5.Text && btn9.Text == "")
+            {
+                btn9.Text = "O";
+                btn9.Enabled = false;
+            }
+            else if (btn7.Text == btn5.Text)
+            {
+                if (btn4.Text == "")
+                {
+                    btn4.Text = "O";
+                    btn4.Enabled = false;
+                }
+                else if (btn8.Text == "")
+                {
+                    btn8.Text = "O";
+                    btn8.Enabled = false;
+                }
+                else if (btn2.Text == "")
+                {
+                    btn2.Text = "O";
+                    btn2.Enabled = false;
+                }
+            }
+        }
+        private void OtherButtons()
+        {
+            if (btn3.Text == "" && (btn1.Text == "X" && btn2.Text == "X"))
+            {
+                btn3.Text = "O";
+                btn3.Enabled = false;
+            }
+            else if (btn2.Text == "" && (btn1.Text == "X" && btn3.Text == "X"))
+            {
+                btn2.Text = "O";
+                btn2.Enabled = false;
+            }
+            else if (btn1.Text == "" && (btn2.Text == "X" && btn3.Text == "X"))
+            {
+                btn1.Text = "O";
+                btn1.Enabled = false;
+            }
+
+
+            else if (btn7.Text == "" && (btn1.Text == "X" && btn4.Text == "X"))
+            {
+                btn7.Text = "O";
+                btn7.Enabled = false;
+            }
+            else if (btn4.Text == "" && (btn1.Text == "X" && btn7.Text == "X"))
+            {
+                btn4.Text = "O";
+                btn4.Enabled = false;
+            }
+            else if (btn1.Text == "" && (btn4.Text == "X" && btn7.Text == "X"))
+            {
+                btn1.Text = "O";
+                btn1.Enabled = false;
+            }
+
+
+            else if (btn9.Text == "" && (btn3.Text == "X" && btn6.Text == "X"))
+            {
+                btn9.Text = "O";
+                btn9.Enabled = false;
+            }
+            else if (btn6.Text == "" && (btn3.Text == "X" && btn9.Text == "X"))
+            {
+                btn6.Text = "O";
+                btn6.Enabled = false;
+            }
+            else if (btn3.Text == "" && (btn6.Text == "X" && btn9.Text == "X"))
+            {
+                btn3.Text = "O";
+                btn3.Enabled = false;
+            }
+
+
+            else if (btn9.Text == "" && (btn7.Text == "X" && btn8.Text == "X"))
+            {
+                btn9.Text = "O";
+                btn9.Enabled = false;
+            }
+            else if (btn8.Text == "" && (btn9.Text == "X" && btn7.Text == "X"))
+            {
+                btn8.Text = "O";
+                btn8.Enabled = false;
+            }
+            else if (btn7.Text == "" && (btn8.Text == "X" && btn9.Text == "X"))
+            {
+                btn7.Text = "O";
+                btn7.Enabled = false;
+            }
+            else
+            {
+                if (btn2.Text == "")
+                {
+                    btn2.Text = "O";
+                    btn2.Enabled = false;
+                }
+                else if (btn8.Text == "")
+                {
+                    btn8.Text = "O";
+                    btn8.Enabled = false;
+                }
+                else if (btn4.Text == "")
+                {
+                    btn4.Text = "O";
+                    btn4.Enabled = false;
+                }
+                else if (btn6.Text == "")
+                {
+                    btn6.Text = "O";
+                    btn6.Enabled = false;
+                }
+                else
+                {
+                    if (btn1.Text == "")
+                    {
+                        btn1.Text = "O";
+                        btn1.Enabled = false;
+                    }
+                    else if (btn3.Text == "")
+                    {
+                        btn3.Text = "O";
+                        btn3.Enabled = false;
+                    }
+                    else if (btn7.Text == "")
+                    {
+                        btn7.Text = "O";
+                        btn7.Enabled = false;
+                    }
+                    else if (btn6.Text == "")
+                    {
+                        btn9.Text = "O";
+                        btn9.Enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
